0a7d4962da4fbf66d77e6f03de60e266258fabe9
YevhenKharchenko <evgenvalerych@gmail.com>
2026-02-13 12:07:13 +0200

    Add about, features, gallery, gameplay and uniqueness sections

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -19,17 +19,39 @@ gallerySwiper = new Swiper('.gallery-swiper-container', {
   grabCursor: true,
   breakpoints: {
     1440: {
-      slidesPerView: 3,
-      centeredSlides: false,
-      initialSlide: 0,
-      allowTouchMove: false,
-      grabCursor: false,
+      slidesPerView: 'auto',
+      initialSlide: 1,
     },
   },
 
   on: {
     init(swiper) {
       document.querySelector('.gallery-swiper-container').classList.add('show');
+      updateGalleryArrows(swiper);
+    },
+    slideChange(swiper) {
+      updateGalleryArrows(swiper);
+    },
+    reachEnd(swiper) {
+      galleryRightArrow.disabled = true;
+    },
+    fromEdge(swiper) {
+      galleryRightArrow.disabled = false;
     },
   },
+});
+
+updateGalleryArrows(gallerySwiper);
+
+function updateGalleryArrows(swiper) {
+  galleryLeftArrow.disabled = swiper.isBeginning;
+  galleryRightArrow.disabled = swiper.isEnd;
+}
+
+galleryLeftArrow.addEventListener('click', () => {
+  gallerySwiper.slidePrev();
+});
+
+galleryRightArrow.addEventListener('click', () => {
+  gallerySwiper.slideNext();
 });
